3cff26ec8947b44f1e36ec4778da67cb845ba249
jeromedecoster <github@jeromedecoster.com>
2014-06-09 07:07:31 +0200

    dep

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
 console.log('coucou from bin index')
+var each = require('async-each-series')
+
+each(['foo','bar','baz'], function(el, next) {
+  setTimeout(function () {
+    console.log(el);
+    next();
+  }, Math.random() * 5000);
+}, function (err) {
+  console.log('finished');
+});
